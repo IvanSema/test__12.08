09694a056b8966e163a6cd8a8121d592c611af43
IvanSema <ivan_8d@ukr.net>
2020-08-12 20:43:14 +0300

    temp version

---
+++ js/tabs.js
@@ -0,0 +1,28 @@
+const grid = document.querySelector('.offer__grid'),
+tabs = document.querySelectorAll('.offer__cell'),
+tabsContent = document.querySelectorAll('.offer-tab');
+
+function hideTabs(){
+    tabs.forEach(elem => elem.classList.remove('active'))
+    tabsContent.forEach(elem => elem.classList.remove('active'))
+}
+
+function showTab(i = 0){
+    hideTabs()
+    tabs[i].classList.add('active')
+    tabsContent[i].classList.add('active')
+}
+
+showTab();
+
+
+grid.addEventListener('click', (event) => {
+    const target = event.target;
+    console.log(target);
+    
+    tabs.forEach((elem, index) => {
+        if(elem === target || elem === target.parentElement){
+            showTab(index);
+        }
+    })
+})
